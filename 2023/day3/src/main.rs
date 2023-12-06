4bd7c5064eada1431f0eaab11566efac0fda6beb
nano <emrebzkurt1@gmail.com>
2023-12-06 18:20:28 +0300

    work in progress

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -1,15 +1,28 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
-fn get_number_indeces(line: &str) -> Vec<u32> {
-    let mut number_indices: Vec<u32> = Vec::new();
+fn get_number_indices(line: &str) -> Vec<Vec<u32>> {
+    let mut number_indices: Vec<Vec<u32>> = Vec::new();
+    let mut sub_vec: Vec<u32> = Vec::new();
     for (index, c) in line.chars().enumerate() {
         if c.is_digit(10) {
-            number_indices.push(index.try_into().unwrap());
+            let u32_index: u32 = index.try_into().unwrap();
+            if sub_vec.len() == 0 || sub_vec[sub_vec.len() - 1] - u32_index > 1 {
+                sub_vec.push(u32_index);
+            } else {
+                number_indices.push(sub_vec);
+                sub_vec = Vec::new();
+            }
         }
     }
     number_indices
 }
+
+fn add_numbers_in_line(line: &str) -> u32 {
+    let number_indices: Vec<Vec<u32>> = get_number_indices(&line);
+    0
+}
+        
 
 fn main() -> std::io::Result<()> {
     let file = File::open("input.txt")?;
@@ -17,7 +30,7 @@ fn main() -> std::io::Result<()> {
 
     for line in reader.lines() {
         let line = line.unwrap();
-        get_number_indeces(&line);
+        get_number_indices(&line);
     }
 
     Ok(())
