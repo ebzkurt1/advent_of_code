c12b99c5d1f153819ca0cedff4ee63b9affc66ee
nano <emrebzkurt1@gmail.com>
2023-12-07 13:25:31 +0300

    progress

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -7,19 +7,30 @@ fn get_number_indices(line: &str) -> Vec<Vec<u32>> {
     for (index, c) in line.chars().enumerate() {
         if c.is_digit(10) {
             let u32_index: u32 = index.try_into().unwrap();
-            if sub_vec.len() == 0 || sub_vec[sub_vec.len() - 1] - u32_index > 1 {
+            if sub_vec.is_empty() || u32_index - sub_vec.last().unwrap_or(&0) <= 1 {
                 sub_vec.push(u32_index);
             } else {
                 number_indices.push(sub_vec);
                 sub_vec = Vec::new();
             }
+        } else {
+            if !sub_vec.is_empty() {
+                number_indices.push(sub_vec);
+                sub_vec = Vec::new();
+            }
         }
     }
+    println!("{:?}", number_indices);
     number_indices
 }
 
 fn add_numbers_in_line(line: &str) -> u32 {
     let number_indices: Vec<Vec<u32>> = get_number_indices(&line);
+    for (num_index, sub_vec) in number_indices.enumerate() {
+        for (c_index, c) in num_index.enumerate() {
+        }
+    }
+
     0
 }
         
